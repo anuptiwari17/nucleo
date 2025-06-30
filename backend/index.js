95a78f3a623510bc1608c3045c1e9a482dd25dfd
Anup Tiwari <anuptiwari050@gmail.com>
2025-06-30 21:31:51 +0530

    Working

--- backend/index.js
+++ backend/index.js
@@ -13,4 +13,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
